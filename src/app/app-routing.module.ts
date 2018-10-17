42018ecfb5b5c7fc23c7f9b13ec71fc77207799c
Jessie <mana84910@gmail.com>
2018-10-17 13:22:32 +0800

    設定router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 
-const routes: Routes = [];
+// Component
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+// 無定義的路徑皆導回首頁
+const fullbackRoute: Route = {
+  path: '**',
+  redirectTo: 'dashboard',
+  pathMatch: 'full'
+};
+
+// 主頁
+const dashboardRoute: Route = {
+  path: 'dashboard',
+  component: DashboardComponent,
+  children: []
+};
+
+// 主要Route
+const routes: Routes = [
+  dashboardRoute,
+  fullbackRoute
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, {
+    useHash: true
+  }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
