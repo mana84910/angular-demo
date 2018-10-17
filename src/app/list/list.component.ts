3533a0a9e557fa55a541db53fdea65574198b016
Jessie <mana84910@gmail.com>
2018-10-17 15:26:14 +0800

    將db資料帶入角色清單頁面

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 
+// Service
+import { PersonService } from '../shared/service/person.service';
+
 // interface
 import { Person } from '../shared/interface/person';
 
@@ -15,29 +18,27 @@ export class ListComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'age', 'jobTitle'];
 
   // 角色資料
-  dataSource = new MatTableDataSource<Person>(PERSONNEL_DATA);
+  dataSource: MatTableDataSource<Person> = null;
 
   // 分頁
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-
-  constructor() {}
+  constructor(
+    private personSvc: PersonService
+  ) {}
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
+    this.getPersons();
+  }
+
+  // 取得角色清單
+  getPersons(): void {
+    this.personSvc.getPersons()
+    .subscribe(res => {
+      this.dataSource = new MatTableDataSource<Person>(res);
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
 }
 
-const PERSONNEL_DATA:  Person[] = [
-  {id: 1, name: 'Alice', age: 25, jobTitle: 'UI/UX設計師'},
-  {id: 2, name: 'Bob', age: 32, jobTitle: '工程師 Engineer'},
-  {id: 3, name: 'Chen', age: 28, jobTitle: 'PM'},
-  {id: 4, name: 'Danny', age: 30, jobTitle: '工程師 Engineer'},
-  {id: 5, name: 'Frank', age: 33, jobTitle: '工程師 Engineer'},
-  {id: 6, name: 'Jeff', age: 38, jobTitle: '經理 Manger'},
-  {id: 7, name: 'Jasmine', age: 23, jobTitle: '專案助理 Project Assistant'},
-  {id: 8, name: 'Keven', age: 30, jobTitle: '系統分析師 SA'},
-  {id: 9, name: 'Larry', age: 35, jobTitle: '工程師 Engineer'},
-  {id: 10, name: 'Nelson', age: 35, jobTitle: 'PM'}
-];
