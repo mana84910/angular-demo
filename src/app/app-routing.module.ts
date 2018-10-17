9fb272aa331e4a4e74a34e3fac62bb4a9ee05a9a
Jessie <mana84910@gmail.com>
2018-10-17 14:40:17 +0800

    新增角色清單頁面、設定頁面route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes, Route } from '@angular/router';
 // Component
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
 
 // 無定義的路徑皆導回首頁
 const fullbackRoute: Route = {
@@ -29,7 +30,12 @@ const loginRoute: Route = {
 const dashboardRoute: Route = {
   path: 'dashboard',
   component: DashboardComponent,
-  children: []
+  children: [
+    {
+      path: 'list',
+      component: ListComponent
+    }
+  ]
 };
 
 // 主要Route
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,16 @@ import {
   MatSidenavModule,
   MatIconModule,
   MatListModule,
-  MatCardModule
+  MatCardModule,
+  MatGridListModule,
+  MatMenuModule
 } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
 
 // Component
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
 
 // Module
 import { AppRoutingModule } from './app-routing.module';
@@ -29,7 +32,8 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
@@ -42,6 +46,8 @@ import { SharedModule } from './shared/shared.module';
     MatListModule,
     MatCardModule,
     MatInputModule,
+    MatGridListModule,
+    MatMenuModule,
     AppRoutingModule,
     SharedModule,
     FormsModule,
